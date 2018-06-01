697b93ad03581863c78126c5de156bf601151ecc
jones6210 <mj6930@btinternet.com>
2018-06-01 21:18:04 +0100

    Echo current postition to fiel in /tmp

--- scripts/move_monitor.sh
+++ scripts/move_monitor.sh
@@ -2,15 +2,21 @@
 
 xrandr --output HDMI3 --off --dpi 200
 
+if [ -e /tmp/monitor_pos.txt ]; then
+		sed '1d' /tmp/monitor_pos.txt > /tmp/monitor_pos.txt
+fi
+
 if [ "$1" == "--left" ]; then
 		xrandr --output eDP1 --mode 2880x1800_60.00 --pos 3264x36 --output HDMI3 --auto --scale 1.7x1.7 --pos 0x0 --fb 6144x1836
 		feh --bg-scale ~/Images/sunset_winter_railway_2880x1800.jpg
+		echo left >> /tmp/monitor_pos.txt
 
 elif [ "$1" == "--right" ]; then
 		 xrandr --output eDP1 --mode 2880x1800_60.00 --pos 0x0 --output HDMI3 --auto --scale 1.7x1.7 --pos 2880x0 --panning 3230x1836+2880+0 --fb 6144x1836
 		feh --bg-scale ~/Images/sunset_winter_railway_2880x1800.jpg ~/Images/desktopimages.jpg
+		echo right >> /tmp/monitor_pos.txt
 
 elif [ "$1" == "--up" ]; then
 		xrandr --output eDP1 --mode 2800x1800_60.00 --pos 192x1836 --output HDMI3 --auto --scale 1.7x1.7 --pos 0x0 --fb 3264x3636
-		
+		echo above >> /tmp/monitor_pos.txt
 fi
