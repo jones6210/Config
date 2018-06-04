a4f1dc3c09d3c0ecbfb3edc04c2809c65a55089a
jones6210 <mj6930@btinternet.com>
2018-06-04 15:03:55 +0100

    Removed file extensions

--- scripts/move_monitor.sh
+++ scripts/move_monitor.sh
@@ -10,7 +10,7 @@ if [ "$1" == "--left" ]; then
 elif [ "$1" == "--right" ]; then
 		 xrandr --output eDP1 --mode 2880x1800_60.00 --pos 0x0 --output HDMI3 --auto --scale 1.7x1.7 --pos 2880x0 --panning 3230x1836+2880+0 --fb 6144x1836
 		feh --bg-scale ~/Images/sunset_winter_railway_2880x1800.jpg ~/Images/desktopimages.jpg
-		echo right > /tmp/monitor_pos
+echo right > /tmp/monitor_pos
 
 elif [ "$1" == "--up" ]; then
 		xrandr --output eDP1 --mode 2800x1800_60.00 --pos 192x1836 --output HDMI3 --auto --scale 1.7x1.7 --pos 0x0 --fb 3264x3636
